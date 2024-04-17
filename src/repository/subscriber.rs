79b95e06a6b23ac805e3d29b2613215b499ea572
farrelayman09 <farrel.ayman09@gmail.com>
2024-04-17 12:36:30 +0700

    Correct compile error from Subcriber instead of Subscriber (Module Part 1)

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -4,7 +4,7 @@ use crate::model::subscriber::Subscriber;
 
 // Singleton of Database
 lazy_static! {
-    static ref SUBSCRIBERS: DashMap<String, DashMap<String, Subcriber>> = DashMap::new();
+    static ref SUBSCRIBERS: DashMap<String, DashMap<String, Subscriber>> = DashMap::new();
 }
 
 pub struct SubscriberRepository;
